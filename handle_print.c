ce55d293d3dd45ac617b22b166b16963f12b455a
shreen-youssif <shreenyoussif22@gmail.com>
2023-09-20 06:03:37 -0700

    new4

--- handle_print.c
+++ handle_print.c
@@ -1,49 +1,49 @@
 #include "main.h"
 /**
- * handle_print - Function that Prints an argument according to its type
- * @format: Format argument
+ * handle_print -Function that prints an argument according to its type
+ * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
  * @ind: ind.
  * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
+ * @flags: active flags
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
  * Return: 1 or 2;
  */
-int handle_print(const char *format, int *ind, va_list list, char buffer[],
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int i, unknown_len = 0, printed_chars = -1;
-	format_t format_types[] = {
+	int i, unknow_len = 0, printed_chars = -1;
+	format_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 		{'X', print_HEXA}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (i = 0; format_types[i].format != '\0'; i++)
-		if (format[*ind] == format_types[i].format)
-			return (format_types[i].fn(list, buffer, flags, width, precision, size));
+	for (i = 0; fmt_types[i].format != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].format)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
-	if (format_types[i].format == '\0')
+	if (fmt_types[i].format == '\0')
 	{
-		if (format[*ind] == '\0')
+		if (fmt[*ind] == '\0')
 			return (-1);
-		unknown_len += write(1, "%%", 1);
-		if (format[*ind - 1] == ' ')
-			unknown_len += write(1, " ", 1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*ind);
-			while (format[*ind] != ' ' && format[*ind] != '%')
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
 				--(*ind);
-			if (format[*ind] == ' ')
+			if (fmt[*ind] == ' ')
 				--(*ind);
 			return (1);
 		}
-		unknown_len += write(1, &format[*ind], 1);
-		return (unknown_len);
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
 	}
 	return (printed_chars);
 }
