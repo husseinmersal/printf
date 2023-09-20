d63632ea0aff90768dda198179d5955d970d773e
shreen-youssif <shreenyoussif22@gmail.com>
2023-09-19 17:15:40 -0700

    get_size.c file

---
+++ get_size.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * get_size - function that gives size 
+ * @format: format argument
+ * @i: the printed list
+ *
+ * Return: Precision.
+ */
+int get_size(const char *format, int *i)
+{
+	int current_i = *i + 1;
+	int size = 0;
+
+	if (format[current_i] == 'l')
+		size = S_LONG;
+	else if (format[current_i] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*i = current_i - 1;
+	else
+		*i = current_i;
+
+	return (size);
+}
